bd44e8b9bc9fd59fa4dc7f81303e182160a1fc70
Archie Strange <archie.strange@wundermanthompson.com>
2022-12-04 17:48:14 +0000

    yarn start

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require("mongodb");
+
 const Db = process.env.ATLAS_URI;
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -8,7 +10,7 @@ const client = new MongoClient(Db, {
 var _db;
 
 module.exports = {
-  connectToServer: (callback) =>{
+  connectToServer: (callback) => {
     client.connect((err, db) => {
       // Verify we got a good "db" object
       if (db) {
