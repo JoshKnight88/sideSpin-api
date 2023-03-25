81b94fa5a0ae7481b8b37c364b8663ede91101a9
Archie Strange <jngknight@gmail.com>
2023-03-25 10:00:54 +0000

    default league search

--- server/routes/player.js
+++ server/routes/player.js
@@ -91,4 +91,15 @@ playerRoutes.route('/codes/:id').get((req, res) => {
     });
 });
 
+playerRoutes.route('/players/default').get((req, res) => {
+  let db_connect = dbo.getDb('sideSpin');
+  db_connect
+    .collection('players')
+    .find({ name: 'Fiver', name: 'Bigwig', name: 'Blackberry', name: 'Dandelion', name: 'Hazel' })
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
 module.exports = playerRoutes;
