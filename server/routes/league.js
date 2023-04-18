4c4fb041866a8b7042ac2e1d80dcb780a0d03e85
Archie Strange <jngknight@gmail.com>
2023-04-18 18:53:16 +0000

    minor syntax edit

--- server/routes/league.js
+++ server/routes/league.js
@@ -34,7 +34,7 @@ leagueRoutes.route('/league/add').post((req, response) => {
 
 leagueRoutes.route('/league/:id').get((req, res) => {
   let db_connect = dbo.getDb('sideSpin');
-  let league = {accessCode: req.params.id};
+  let league = { accessCode: req.params.id };
   db_connect.collection('leagues').findOne(league, (err, result) => {
     if (err) throw err;
     res.json(result);
